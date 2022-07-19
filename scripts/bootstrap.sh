a4322a060f65b627fdfdbfe9a7e680f0966b714a
Dincho Todorov <dincho@users.noreply.github.com>
2022-07-19 20:24:49 +0300

    Change argocd applications strcuture to allow values overwriting

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,4 @@
 
 (cd argocd && helm dep up)
 
-helm upgrade --install --atomic -n argocd -f argocd/values.yaml -f argocd/values-dev.yaml -f argocd/values-bootstrap.yaml argocd ./argocd/
+helm upgrade --install --atomic --create-namespace --namespace argocd -f argocd/values.yaml -f argocd/values-dev.yaml -f argocd/values-bootstrap.yaml argocd ./argocd/
